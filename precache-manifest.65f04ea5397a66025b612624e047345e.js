f2b79acb1d99abe5e465073287fd6d96bf8792be
Jimmy in Rain <quangminhit94@gmail.com>
2020-01-04 01:41:42 +0700

    Updates

---
+++ precache-manifest.65f04ea5397a66025b612624e047345e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60aa4ad408221accdea1e2ab0641d400",
+    "url": "/deploy-react-github-pages/index.html"
+  },
+  {
+    "revision": "6d6df4975f33dbb6056c",
+    "url": "/deploy-react-github-pages/static/css/main.f1b42223.chunk.css"
+  },
+  {
+    "revision": "2bc470559fb0f2b93b06",
+    "url": "/deploy-react-github-pages/static/js/2.99142b2e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/deploy-react-github-pages/static/js/2.99142b2e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d6df4975f33dbb6056c",
+    "url": "/deploy-react-github-pages/static/js/main.cd567c68.chunk.js"
+  },
+  {
+    "revision": "a7737cd323b0cf3bc6b8",
+    "url": "/deploy-react-github-pages/static/js/runtime-main.2c314a5c.js"
+  }
+]);
